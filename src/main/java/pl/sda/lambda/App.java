d4d2b3413e41253c9b6b8b9c65c58aac7280af01
Emilia Zawisza <emilia.zawisza@wp.pl>
2019-08-04 14:13:20 +0200

    lambda

--- src/main/java/pl/sda/lambda/App.java
+++ src/main/java/pl/sda/lambda/App.java
@@ -1,13 +1,34 @@
 package pl.sda.lambda;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.util.*;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<String> names = Arrays.asList("Kasia", "Ania", "Zosia","Bartek");
+
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        Collections.sort(names, (String s1, String s2)-> s1.compareTo(s2)); // funkcja przekazana w argumencie
+        System.out.println(names);
+
+
+
+        Collections.sort(SampleData.membersOfTheBeatles, new Comparator<Artist>() {
+            @Override
+            public int compare(Artist o1, Artist o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        System.out.println(SampleData.membersOfTheBeatles);
+
     }
 }
